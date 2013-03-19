84d5f283748225426c91271c5325f9f0efcf5253
Tomasz Jaholkowski <jahol@ais.pl>
2013-03-19 18:59:27 +0100

    world coding start

---
+++ source/Mud.Core/MultiThreadObject.cs
@@ -0,0 +1,7 @@
+﻿namespace Mud.Core
+{
+    public abstract class MultiThreadObject
+    {
+        protected readonly object LockVar = new object();
+    }
+}
---
+++ source/Mud.World/Thing.cs
@@ -0,0 +1,36 @@
+﻿namespace Mud.World
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Mud.Core;
+
+    public class Thing : MultiThreadObject
+    {
+        private readonly List<Thing> childs;
+
+        private Thing parent;
+
+        public Thing()
+        {
+            this.childs = new List<Thing>();
+        }
+
+        public IEnumerable<Thing> Childs
+        {
+            get
+            {
+                return this.childs;
+            }
+        }
+
+        public void Insert(Thing child)
+        {
+            // TODO: remove from prev parent
+            child.parent = this;
+            this.childs.Add(child);
+        }
+    }
+}
