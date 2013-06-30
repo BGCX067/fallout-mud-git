85bded7fecc1f077ec1116269cf126592677d274
Tomasz Jaholkowski <jahol@ais.pl>
2013-06-30 12:36:09 +0200

    minor

---
+++ source/Mud.Core/Session/Session.cs
@@ -0,0 +1,18 @@
+﻿namespace Mud.Core.Session
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Mud.Abstractions.Communication;
+
+    public class Session
+    {
+        public Session(IConnection connection)
+        {
+
+        }
+    }
+}
